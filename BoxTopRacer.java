2ce5286446944e14e79ced7251ac0cc1bec3de55
Kent Collins <kent_collins@asl.org>
2014-11-13 08:44:32 +0000

    key complete

--- BoxTopRacer.java
+++ BoxTopRacer.java
@@ -7,12 +7,29 @@
  */
 public class BoxTopRacer extends Racer {
 	
+	public BoxTopRacer(int y) {
+		super(y);
+	}
+
 	/**
 	 * Re-implement the method to allow for jumping hurdles of any width
 	 */
 	@Override
 	public void jumpRight() {
-		
+		turnLeft();
+		while (!this.rightIsClear()) {
+			move();
+		}
+		turnRight();
+		move();
+		while(!this.rightIsClear()){
+			move();
+		}
+		turnRight();
+		while (this.frontIsClear()) {
+			move();
+		}
+		turnLeft();
 	}
 
 }
--- SteepleChaseRacer.java
+++ SteepleChaseRacer.java
@@ -7,12 +7,26 @@
  */
 public class SteepleChaseRacer extends Racer {
 	
+	public SteepleChaseRacer(int y) {
+		super(y);
+	}
+
 	/**
 	 * Re-implement the method to allow for jumping hurdles of any height
 	 */
 	@Override
 	public void jumpRight() {
-		
+		turnLeft();
+		while (!this.rightIsClear()) {
+			move();
+		}
+		turnRight();
+		move();
+		turnRight();
+		while (this.frontIsClear()) {
+			move();
+		}
+		turnLeft();
 	}
 
 }
